32b06906670eb9b080c3e69ffe85375f0eadb4c1
Guilherme Moreira <guilhermemoreira@geekplug.com.br>
2021-05-05 11:08:00 -0300

    :sparkles:Add a TypeORM in backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const typeORM = TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgrespass',
+  database: 'leleluia',
+  entities: [],    
+  synchronize: true,
+})
+
+const env = ConfigModule.forRoot({
+  envFilePath: '.env'
+})
+
 @Module({
-  imports: [],
+  imports: [typeORM, env],
   controllers: [AppController],
   providers: [AppService],
 })
