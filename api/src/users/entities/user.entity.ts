fd6987e88b11e9f10e0d66a5b96e2a9bd37655d3
Guilherme Moreira <guilhermemoreira@geekplug.com.br>
2021-05-07 18:18:56 -0300

    🔥Create Users

---
+++ api/src/users/entities/user.entity.ts
@@ -0,0 +1,28 @@
+import { Profile } from "src/profile/entities/profile.entity"
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column({
+        type: "text",
+        nullable: false,
+        unique: true
+    })
+    email:  string
+
+    @Column({
+        type: "text",
+        nullable: false,
+    })
+    password:  string
+
+    @OneToOne(() => Profile, {
+        eager: true,
+        cascade: true,
+    })
+    @JoinColumn()
+    profile: Profile
+}
